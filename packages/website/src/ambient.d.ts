1be1f484cb7fac79ca053598d628943a600f077a
Gaurav Khanna <gauravk92@gmail.com>
2025-08-11 10:13:32 -0700

    save

---
+++ packages/website/src/ambient.d.ts
@@ -0,0 +1,4 @@
+declare let setInterval: WindowOrWorkerGlobalScope['setInterval']
+declare let clearInterval: WindowOrWorkerGlobalScope['clearInterval']
+declare let setTimeout: WindowOrWorkerGlobalScope['setTimeout']
+declare let clearTimeout: WindowOrWorkerGlobalScope['clearTimeout']

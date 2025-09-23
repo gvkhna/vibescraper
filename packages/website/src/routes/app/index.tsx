2410917c5b1b7adbfc62f592ea04c3fb52edcd8d
Gaurav Khanna <gauravk92@gmail.com>
2025-09-23 16:08:39 -0700

    fix error

--- packages/website/src/routes/app/index.tsx
+++ packages/website/src/routes/app/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/app/')({
   beforeLoad: () => {
-    return redirect({ to: '/' })
+    redirect({ to: '/' })
   }
 })
